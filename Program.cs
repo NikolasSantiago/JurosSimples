67e5ac4ed33218c9e333b3c79e95190e41bbc30e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 20:50:03 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+double capital, tempo, juros, total;
+
+Console.Clear();
+
+Console.WriteLine("capital {c}______");
+capital = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("juros {j} (%)______:");
+juros = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("tempo {t}______");
+tempo = Convert.ToDouble(Console.ReadLine());
+
+juros = capital * (juros /100) * tempo;
+
+total = capital + juros;
+
+Console.WriteLine($"montante (R$)..: {total}");
+
